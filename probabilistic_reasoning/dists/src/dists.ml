318b1a5ae75a75b5d200250606a5ef4082abe5ab
Lewis Hammond <lewis@lewishammond.com>
2019-08-12 21:07:48 +0100

    Update MCMC algorithm

--- probabilistic_reasoning/dists/src/dists.ml
+++ probabilistic_reasoning/dists/src/dists.ml
@@ -1,4 +1,4 @@
-(* dists.ml - A module for sampling from a range of probability distributions parameterised by the user
+(* dists.ml - A module for sampling from a range of probability distributions parameterised/linearly constrained by the user
 Lewis Hammond - lewis@imandra.ai
 2019 *)
 
@@ -17,20 +17,11 @@ let pi = 4. *. atan 1.
 
 let bounded x = x >= 0. && x <= 1.
 
-let rec sum l =
-  match l with
-    | [] -> 0.
-    | h :: t -> h +. (sum t)
+let sum l = List.fold_left (+.) 0. l
  
-let normalise l =
-  let s = sum l in
-  let rec div n m =
-    match n with
-      | [] -> m
-      | h :: t -> div t ((h /. s) :: m)
-  in div l []
+let normalise l = let s = (sum l) in List.map (fun x -> x /. s) l
 
-let rec log_factorial x =
+let log_factorial x =
   let rec loop i a =
     if i > x then a
     else loop (i +. 1.) (a +. log i)
@@ -45,20 +36,17 @@ let choose n k =
 let gamma _ = failwith "TODO"
 
 let constrain_categorical (constraints) (classes, probs) =
-  if constraints = [] then 
-    classes, probs
-  else
-	let rec loop old_c old_p new_c new_p =
-	  match old_c, old_p with
-		| [], _ -> new_c, new_p
-		| _, [] -> new_c, new_p
-		| c :: cs, p :: ps ->
-		  if List.mem c constraints then
-			loop cs ps (c :: new_c) (p :: new_p)
-		  else
-			loop cs ps new_c new_p
-	in let new_c, new_p = loop classes probs [] [] in
-	new_c, (normalise new_p)
+  let rec loop old_c old_p new_c new_p =
+	match old_c, old_p with
+	  | [], _ -> new_c, new_p
+	  | _, [] -> new_c, new_p
+	  | c :: cs, p :: ps ->
+		if List.mem c constraints then
+		  loop cs ps (c :: new_c) (p :: new_p)
+		else
+		  loop cs ps new_c new_p
+  in let new_c, new_p = loop classes probs [] [] in
+  new_c, (normalise new_p)
 
 let get_uniform_constraints rs cdf =
   let rec loop rs u_rs w_rs =
@@ -69,44 +57,65 @@ let get_uniform_constraints rs cdf =
 		let w = c_b -. c_a in
 		loop t ((c_a, c_b) :: u_rs) (w :: w_rs)
   in let (u_rs, w_rs) = loop rs [] [] in
-  let n_w_rs = normalise w_rs in
-  u_rs, n_w_rs
+  u_rs, (normalise w_rs)
+
+let closest m cs =
+  let rec loop curr dist list =
+    match list with
+      | [] -> curr
+      | h :: t -> 
+        let (a, b) = h in
+        if a <= m && m <= b then
+          m
+        else
+          let a_dist = abs_float (a -. m) in
+          let b_dist = abs_float (b -. m) in
+          if dist = 0. then
+            if a_dist < b_dist then loop a a_dist t
+            else loop b b_dist t
+          else if a_dist < dist then 
+            if a_dist < b_dist then loop a a_dist t
+            else loop b b_dist t
+          else if b_dist < dist then
+            loop b b_dist t
+          else
+            loop curr dist t
+  in loop 0. 0. cs
   
 
 (* Primitive random elements *)
 
-let rand_bool = 
-  Random.bool ()
+let rand_bool () = Random.bool ()
 
 let rand_int ?(bound = max_int) () = 
   let i = Random.int bound in
-  if rand_bool then i
+  if rand_bool () then i
   else -i
 
-let rand_Z ?(bound = max_int) () = 
-  let i = rand_int ~bound () in Z.of_int i
+(* let rand_Z ?(bound = max_int) () = 
+  let i = rand_int ~bound () in Z.of_int i *)
 
 let rand_float ?(bound = max_float) () =
   let f = Random.float bound in
-  if rand_bool then f
+  if rand_bool () then f
   else -.f
 
-let rand_Q ?(bound = max_float) () =
-  let f = rand_float ~bound () in Q.of_float f
+(* let rand_Q ?(bound = max_float) () =
+  let f = rand_float ~bound () in Q.of_float f *)
 
 let base ?(a = 0.) ?(b = 1.) () = 
-  let () = assert (a <= b) in
-  a +. Random.float (b -. a)
+  if a <= b then a +. Random.float (b -. a)
+  else failwith "Cannot sample from base of negative range"
 
 
 (* Quantile functions *)
 
 let q_bernoulli x ~p:p = 
-  if x < p then true else false
+  if x <= p then true else false
 
 let q_binomial x ~n:n ~p:p =
   let rec get_successes k prob =
-    let term = (choose n k) *. (p ** k) *. ((1.0 -. p) ** (n -. 1.0)) in
+    let term = (choose n k) *. (p ** k) *. ((1. -. p) ** (n -. 1.)) in
     if x <= term +. prob then int_of_float k
     else get_successes (k +. 1.) (term +. prob)
   in get_successes 0. 0.
@@ -125,16 +134,14 @@ let q_cauchy x ~x_0:x_0 ~gamma:gamma =
   x_0 +. (gamma *. tan(pi *. (x -. 0.5)))
 
 let q_exponential x ~lambda:lambda = 
-  -.(log (1.0 -. x)) /. lambda
+  -.(log (1. -. x)) /. lambda
 
 let q_laplace x ~mu:mu ~b:b = 
-  if x <= 0.5 then
-    mu +. (b *. log (2.0 *. x))
-  else
-    mu -. (b *. log (2.0 -. (2.0 *. x)))
+  if x <= 0.5 then mu +. (b *. log (2. *. x))
+  else mu -. (b *. log (2. -. (2. *. x)))
 
 let q_logistic x ~mu:mu ~s:s = 
-  mu +. (s *. log (x /. (1.0 -. x)))
+  mu +. (s *. log (x /. (1. -. x)))
 
 let q_poisson x ~lambda:lambda =
   let rec get_successes k prob =
@@ -158,7 +165,7 @@ let c_binomial x ~n:n ~p:p =
     if k > x then 
       prob
     else
-      let term = (choose n k) *. (p ** k) *. ((1.0 -. p) ** (n -. 1.0)) in
+      let term = (choose n k) *. (p ** k) *. ((1. -. p) ** (n -. 1.)) in
       get_prob (k +. 1.) (term +. prob)
   in get_prob 0. 0.
 
@@ -179,10 +186,8 @@ let c_exponential x ~lambda:lambda =
   1. -. exp (-.lambda *. x) 
 
 let c_laplace x ~mu:mu ~b:b = 
-  if x <= mu then
-    0.5 *. exp ((x -. mu) /. b)
-  else
-    1. -. (0.5 *. exp (-. (x -. mu) /. b))
+  if x <= mu then 0.5 *. exp ((x -. mu) /. b)
+  else 1. -. (0.5 *. exp (-. (x -. mu) /. b))
 
 let c_logistic x ~mu:mu ~s:s = 
   1. /. (1. +. exp (-. (x -. mu) /. s))
@@ -198,15 +203,12 @@ let c_poisson x ~lambda:lambda =
   in get_prob 0. 0.
 
 let c_uniform x ~a:a ~b:b =
-  if x < a then
-    0.
-  else if x > b then
-    1.
-  else 
-    (x -. a) /. (b -. a) 
+  if x < a then 0.
+  else if x > b then 1.
+  else (x -. a) /. (b -. a) 
 
 
-(* Define density functions for MCMC sampling *)
+(* Density functions *)
 
 let d_beta x ~a:a ~b:b =
   let z = (gamma a) *. (gamma b) /. gamma (a +. b) in
@@ -225,7 +227,7 @@ let d_lognormal x ~mu:mu ~sigma:sigma =
   exp (d_gaussian x ~mu:mu ~sigma:sigma)
 
 
-(* Define sampling functions *)
+(* Sampling algorithms *)
 
 let inverse_transform_sample qf (u_rs, n_w_rs) =
   match u_rs, n_w_rs with
@@ -233,7 +235,8 @@ let inverse_transform_sample qf (u_rs, n_w_rs) =
 	| _, _ -> let (r1, r2) = q_categorical (base ()) ~classes:u_rs ~probs:n_w_rs in
               base ~a:r1 ~b:r2 () |> qf
     
-let mcmc_sample pdf constraints cur_x step =
+let mcmc_sample pdf bounds constraints cur_x step =
+  let (upper, lower) = bounds in
   let rec apply_constraints rs prev p =
     let (a, b) = prev in
     match rs with
@@ -254,18 +257,18 @@ let mcmc_sample pdf constraints cur_x step =
   let rec get_proposal () =
     let epsilon = base ~a:(-.step) ~b:step () in
     let new_x = cur_x +. epsilon in
-    if constraints = [] then
+    if new_x < lower || new_x > upper then
+      get_proposal ()
+    else if constraints = [] then
       new_x
     else
       let result = apply_constraints constraints (None, None) new_x in
       match result with
         | None -> get_proposal ()
-        | Some f -> f 
-      in
-  let new_x = get_proposal () in
+        | Some f -> f
+  in let new_x = get_proposal () in
   let ratio = (pdf new_x) /. (pdf cur_x) in
-  let u = base () in
-  if u <= ratio then new_x else cur_x
+  if base () <= ratio then new_x else cur_x
 
 
 (* Sampler functor and related types *)
@@ -282,6 +285,7 @@ end
 module type MCMC_S = sig
   type value
   val pdf : float -> float
+  val bounds : float * float
   val start : float
   val step : float
   val constraints : (float * float) list
@@ -290,11 +294,11 @@ end
 module Sampler = struct
   module type S = sig
     type value
-    val sample : ?start:float -> ?step:float -> int -> value list
+    (* val sample : ?start:float -> ?step:float -> ?burn_in:int -> int -> value list *)
   end
   module Make_Inverse_Transform (D : Inverse_Transform_S) : S with type value = D.value = struct
     type value = D.value
-    let sample ?start:_ ?step:_ n =
+    let sample ?start:_ ?step:_ ?burn_in:_ n =
       let rec loop samples num =
         match num with
           | 0 -> samples
@@ -305,19 +309,37 @@ module Sampler = struct
   end
   module Make_MCMC (D : MCMC_S) : S with type value = float = struct
     type value = float
-    let sample ?(start = D.start) ?(step = D.step) n =
-      let rec loop samples num =
+    let sample ?(start = D.start) ?(step = D.step) ?(burn = 1000) n =
+      let rec burn_in num acceptance_rates rate prev_10 last b_step =
+        match num with
+          | 0 -> (last, b_step)
+          | _ ->               
+            let s = mcmc_sample D.pdf D.bounds D.constraints last b_step in
+            let r = if s = last then 0.01 else 0. in
+            let new_10 = r +. prev_10 in
+            if (num - 1) mod 10 = 0 then
+              let new_acceptance_rates = new_10 :: acceptance_rates in
+              if (num - 1) mod 100 = 0 then
+                let new_rate = rate +. new_10 -. (List.nth acceptance_rates 10) in
+                let diff = 1. +. (new_rate -. 0.25) in
+                burn_in (num - 1) new_acceptance_rates new_rate 0. s (diff *. b_step)
+              else
+                burn_in (num - 1) new_acceptance_rates (rate +. new_10) 0. s b_step
+            else
+              burn_in (num - 1) acceptance_rates rate new_10 s b_step
+      in let (burn_in_start, burn_in_step) = burn_in burn [] step start 0. step in
+      let rec loop samples num last =
         match num with
           | 0 -> samples
           | _ -> 
-            let s = mcmc_sample D.pdf D.constraints start step in 
-            loop (s :: samples) (num - 1)
-      in loop [] n
+            let s = mcmc_sample D.pdf D.bounds D.constraints last burn_in_step in 
+            loop (s :: samples) (num - 1) s
+      in loop [] n burn_in_start
     end
 end
 
 
-(* Distributions *)
+(* Distribution modules *)
 
 module Bernoulli 
   (Params : sig val p : float end) 
@@ -339,23 +361,23 @@ module Beta
   (Params : sig val a : float val b : float end) 
   (Constraints : sig val c : (float * float) list option end)
   : Sampler.S with type value = float = struct
-    assert (Params.a > 0. 
-            && Params.b > 0.)
+    assert (Params.a > 0. &&
+            Params.b > 0.)
     include Sampler.Make_MCMC (struct
       type value = float
       let pdf x = d_beta x ~a:Params.a ~b:Params.b
+      let bounds = (0., 1.)
       let constraints = 
         match Constraints.c with
           | None -> []
           | Some l -> l
-      let step = 0.005
+      let step =
+        let std = sqrt ((Params.a *. Params.b) /. ((Params.a +. Params.b) ** 2.) *. (Params.a +. Params.b +. 1.)) in
+        0.05 *. std
       let start = 
-        if constraints = [] then 
-          Params.a /. (Params.a +. Params.b)
-        else
-          let _, weights = get_uniform_constraints constraints (fun x -> x) in
-          let (r1, r2) = q_categorical (base ()) ~classes:constraints ~probs:weights in
-          (r1 +. r2) /. 2.
+        let mean = Params.a /. (Params.a +. Params.b) in
+        if constraints = [] then mean
+        else closest mean constraints
     end)
 end
 
@@ -382,9 +404,10 @@ module Categorical
   (Constraints : sig type t = Params.t val c : t list option end)
   : Sampler.S with type value = Params.t = struct
     assert (Params.classes <> [] &&
+            Constraints.c <> Some [] &&
             List.length Params.classes = List.length Params.probs &&
             List.for_all bounded Params.probs &&
-            sum Params.probs = 1.0)
+            sum Params.probs = 1.)
     include Sampler.Make_Inverse_Transform (struct
       type value = Params.t
       let constraints = [], []
@@ -438,18 +461,18 @@ module Gamma
     include Sampler.Make_MCMC (struct
       type value = float
       let pdf x = d_gamma x ~k:Params.k ~theta:Params.theta
+      let bounds = (0., max_float)
       let constraints = 
         match Constraints.c with
           | None -> []
           | Some l -> l
-      let step = 0.05
+      let step =
+        let std = Params.theta *. sqrt Params.k in
+        0.05 *. std
       let start = 
-        if constraints = [] then 
-          base () ~a:0. ~b:max_float
-        else
-          let _, weights = get_uniform_constraints constraints (fun x -> x) in
-          let (r1, r2) = q_categorical (base ()) ~classes:constraints ~probs:weights in
-          (r1 +. r2) /. 2.
+        let mean = Params.k *. Params.theta in
+        if constraints = [] then mean
+        else closest mean constraints
     end)
 end
 
@@ -461,18 +484,18 @@ module Gaussian
     include Sampler.Make_MCMC (struct
       type value = float
       let pdf x = d_gaussian x ~mu:Params.mu ~sigma:Params.sigma
+      let bounds = (min_float, max_float)
       let constraints = 
         match Constraints.c with
           | None -> []
           | Some l -> l
-      let step = 0.05
+      let step =
+        let std = Params.sigma in
+        0.05 *. std
       let start = 
-        if constraints = [] then 
-          Params.mu
-        else
-          let _, weights = get_uniform_constraints constraints (fun x -> x) in
-          let (r1, r2) = q_categorical (base ()) ~classes:constraints ~probs:weights in
-          (r1 +. r2) /. 2.
+        let mean = Params.mu in
+        if constraints = [] then mean
+        else closest mean constraints
     end)
 end
 
@@ -516,18 +539,18 @@ module LogNormal
     include Sampler.Make_MCMC (struct
       type value = float
       let pdf x = d_lognormal x ~mu:Params.mu ~sigma:Params.sigma
+      let bounds = (0., max_float)
       let constraints = 
         match Constraints.c with
           | None -> []
           | Some l -> l
-      let step = 0.05
+      let step = 
+        let std = (exp (Params.sigma ** 2.) -. 1.) *. exp ((2. *. Params.mu) +. (Params.sigma ** 2.)) in
+        0.05 *. std
       let start = 
-        if constraints = [] then 
-          exp (Params.mu +. ((Params.sigma ** 2.) /. 2.))
-        else
-          let _, weights = get_uniform_constraints constraints (fun x -> x) in
-          let (r1, r2) = q_categorical (base ()) ~classes:constraints ~probs:weights in
-          (r1 +. r2) /. 2.
+        let mean = exp (Params.mu +. ((Params.sigma ** 2.) /. 2.)) in
+        if constraints = [] then mean
+        else closest mean constraints
     end)
 end
 
@@ -566,5 +589,4 @@ end
 
 (* Example user code *)
 
-module My_bernoulli = Bernoulli (struct let p = 0.5 end) (struct let c = None end)
-
+module My_Bernoulli = Bernoulli (struct let p = 0.5 end) (struct let c = None end)
